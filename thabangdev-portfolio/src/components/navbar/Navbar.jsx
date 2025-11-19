f007486ba5a3e8257ac29a674134ea293bcefbac
thabangDev77 <phoshokothabang20@gmail.com>
2025-11-19 19:27:19 +0200

    style: update navbar styling for improved layout and appearance

--- thabangdev-portfolio/src/components/navbar/Navbar.jsx
+++ thabangdev-portfolio/src/components/navbar/Navbar.jsx
@@ -3,10 +3,10 @@ import NavLink from './NavLink'
 
 function Navbar() {
   return (
-    <nav>
-        <h1>Thabang.Dev</h1>
+    <nav className='grid grid-flow-col gap-2 m-3 p-2 border-2 rounded-xl'>
+        <h1 className='text-2xl font-bold italic mt-1'>Thabang.Dev</h1>
         <NavLink />
-        <button>Contact</button>
+        <button className='border rounded-2xl m-1 p-1 bg-blue-500'>Contact</button>
     </nav>
   )
 }
