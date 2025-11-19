ab49e369676a42002a5e4003403509ba84a13bd7
thabangDev77 <phoshokothabang20@gmail.com>
2025-11-19 18:43:45 +0200

    feat: create the navbar links

---
+++ thabangdev-portfolio/src/components/navbar/NavLink.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function NavLink() {
+  return (
+    <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/exprtise">Expertise</Link></li>
+        <li><Link to="/blog">Blog</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+    </ul>
+  )
+}
+
+export default NavLink
