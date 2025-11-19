15fadf9eacdc0bfbb6ce9a945d10e23653122b6b
thabangDev77 <phoshokothabang20@gmail.com>
2025-11-19 18:50:16 +0200

    feat: create a navbar

---
+++ thabangdev-portfolio/src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavLink from './NavLink'
+
+function Navbar() {
+  return (
+    <nav>
+        <h1>Thabang.Dev</h1>
+        <NavLink />
+        <button>Contact Me</button>
+    </nav>
+  )
+}
+
+export default Navbar
