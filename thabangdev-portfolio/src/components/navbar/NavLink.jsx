27fab2f3fd64e3133ecef8eaba90868547ed3bca
thabangDev77 <phoshokothabang20@gmail.com>
2025-11-19 19:26:12 +0200

    style: update nav links styling for better layout

--- thabangdev-portfolio/src/components/navbar/NavLink.jsx
+++ thabangdev-portfolio/src/components/navbar/NavLink.jsx
@@ -3,13 +3,12 @@ import { Link } from 'react-router-dom';
 
 function NavLink() {
   return (
-    <ul>
+    <ul className='flex justify-around p-2 border-2 rounded-xl'>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/projects">Projects</Link></li>
         <li><Link to="/exprtise">Expertise</Link></li>
         <li><Link to="/blog">Blog</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
     </ul>
   )
 }
