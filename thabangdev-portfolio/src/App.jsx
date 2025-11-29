dec9e231d3128576d4d85e69080fa5a776268d41
thabangDev77 <phoshokothabang20@gmail.com>
2025-11-29 13:46:41 +0200

    chore: do housekeeping by organising the files for easy excess

--- thabangdev-portfolio/src/App.jsx
+++ thabangdev-portfolio/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar';
-import Home from './components/Home';
-import About from './components/About';
+import Home from './components/home/Home';
+import About from './components/about/About';
 import Project from './components/projects/Project';
 import Expertise from './components/expertise/Expertise';
 import Blog from './components/articles/Blog';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
