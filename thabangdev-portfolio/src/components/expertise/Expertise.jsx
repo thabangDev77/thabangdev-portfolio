c8f6ef4801cd4257d3c6959f72057b8793298c10
thabangDev77 <phoshokothabang20@gmail.com>
2025-11-29 12:47:25 +0200

    chore: add an expertise section

---
+++ thabangdev-portfolio/src/components/expertise/Expertise.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Expertise() {
+  return (
+    <section>
+      <h2>Expertise</h2>
+      <p>With a solid foundation in data structures and algorithms, I approach challenges with a structured mindset.<br/> My growing proficiency in JavaScript, HTML, and CSS has enabled me to build dynamic and responsive web applications.</p>
+        {/*
+        <div>skills</div>
+        <div>experience</div>
+        <div>services</div>
+        
+        */}
+        
+
+        <div>
+            <div>
+                <h3>Tech Stack</h3>
+            </div>
+            <div>
+            {/* icons */}
+            </div>
+        </div>
+
+        <div>
+            <div>
+                <h3>Tools</h3>
+            </div>
+            <div>
+              {/* icons */}
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Expertise
