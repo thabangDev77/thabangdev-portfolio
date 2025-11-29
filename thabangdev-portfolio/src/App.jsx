aeb62fdda2b02657df64437ae4e6ec1154fadef4
thabangDev77 <phoshokothabang20@gmail.com>
2025-11-29 11:39:46 +0200

    Chore: add an about me section boilerplate and display it

--- thabangdev-portfolio/src/App.jsx
+++ thabangdev-portfolio/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar';
 import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
     <main>
       <Navbar />
       <Home />
+      <About />
     </main>
     
   )
