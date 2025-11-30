c553fb119942ce0592c9615689239bb6a4a1ed13
thabangDev77 <phoshokothabang20@gmail.com>
2025-11-30 03:17:30 +0200

    feat: add social media icons to the footer section

--- thabangdev-portfolio/src/components/footer/Footer.jsx
+++ thabangdev-portfolio/src/components/footer/Footer.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub, faXTwitter, faDiscord } from '@fortawesome/free-brands-svg-icons';
+
 
 function Footer() {
   return (
@@ -8,7 +12,13 @@ function Footer() {
       <h3>Thabang.Dev</h3>
       <p>Get in touch with me and get the ball rolling</p>
       <div>
-         {/* Social media icons to be added here */} 
+          {/* Social media icons */}
+          <FontAwesomeIcon icon={faEnvelope} size="xl" />
+          <FontAwesomeIcon icon={faLinkedin} size="xl" style={{color: "#33a7ff",}} />
+          <FontAwesomeIcon icon={faGithub} size="xl" />
+          <FontAwesomeIcon icon={faXTwitter} size="xl" style={{color: "#000000",}} />
+          <FontAwesomeIcon icon={faDiscord} size="xl" style={{color: "#9875ff",}} />
+                     
       </div>
       <div>
           <a href="#">Home</a>
