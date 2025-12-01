be260a26e07ece00dccc9eeb5e2a1e1df2a32c25
thabangDev77 <phoshokothabang20@gmail.com>
2025-12-01 23:41:42 +0200

    style: improve ProjectCard layout and styling for better presentation

--- thabangdev-portfolio/src/components/projects/ProjectCard.jsx
+++ thabangdev-portfolio/src/components/projects/ProjectCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
+import projectImage from '../../assets/images/project.jpg'
 
 function ProjectCard() {
   return (
-    <div>
-        <img src="#" alt="#" width="350" height="350" />
-        <br/>
-        <strong>Project Name</strong>
-        <p>Projects description</p>
-        <button>View Project</button>
+    <div className='flex flex-col justify-center items-center gap-1 border-2 rounded-xl p-1'>
+        <img src={projectImage} alt='project photo' style={{ width: '100%', height: 'auto', borderRadius: '10px'}} />
+        <strong className='text-lg italic '>Project Name</strong>
+        <p className='text-sm'>Projects description</p>
+        <button className='text-sm font-bold border rounded-3xl p-2 m-2'>View Project</button>
     </div>
   )
 }
