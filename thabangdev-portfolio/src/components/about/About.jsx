9bf269ed2e6a123229e15c2731783b4ecbf5b16f
thabangDev77 <phoshokothabang20@gmail.com>
2025-12-01 23:23:52 +0200

    style: restructure the layout of the About section with grid display

--- thabangdev-portfolio/src/components/about/About.jsx
+++ thabangdev-portfolio/src/components/about/About.jsx
@@ -6,8 +6,8 @@ function About() {
         <section className='flex flex-col justify-center items-center gap-8 my-20 px-4'>
             <h2 className='text-4xl font-bold'>About Me</h2>
 
-            <div>
-              <div>
+            <div className='grid grid-cols-5 gap-10 items-center ml-10'>
+              <div className='col-span-3'>
                   <p>Hi, I'm Thabang, a dedicated student with a strong interest in full stack web development. I'm passionate about building complete web applications; from crafting responsive front-end interfaces to designing robust back-end systems. I enjoy the challenge of turning ideas into interactive digital experiences that are both functional and visually appealing.</p>
                   <br/>
                   <p>Currently, I'm focused on expanding my skills in technologies like HTML, CSS, JavaScript, Python, and Node.js, while diving deeper into frameworks like React and Express. I love learning by doing; whether it's working on personal projects, contributing to collaborative teams, or exploring open-source code to understand how real-world systems are built.</p>
@@ -15,8 +15,8 @@ function About() {
                   <p>As I grow in my journey as a developer, I'm excited to connect with others, learn continuously, and create impactful solutions. Whether it's a sleek portfolio site or a full-featured web app, I believe in writing clean code and building with purpose.</p>  
               </div>
 
-              <div>
-                  <img src={aboutImage} alt="about me image" style={{ width: '400px', height: '300px'}} /> 
+              <div className='col-span-2 flex justify-center mr-20'>
+                  <img src={aboutImage} alt="about me image" style={{ width: '300px', height: '200px', borderRadius: '20px'}} /> 
               </div>
             </div>
             <button className='border-2 rounded-3xl p-2 font-bold'>Discover More About Me</button>            
