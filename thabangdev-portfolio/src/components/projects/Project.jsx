ff05acfa4ed8757d52a723ef81468d6e020132ff
thabangDev77 <phoshokothabang20@gmail.com>
2025-12-01 23:59:03 +0200

    style: enhance text styling in the Projects section for better readability

--- thabangdev-portfolio/src/components/projects/Project.jsx
+++ thabangdev-portfolio/src/components/projects/Project.jsx
@@ -5,9 +5,9 @@ function Project() {
   return (
     <section  className='flex flex-col justify-center items-center gap-2 my-20 px-4'>
       <h2 className='text-4xl font-bold'>Recent Projects</h2>
-      <p>Below are some of my recent work.Check out my complete <a href="#" target="_blank">portfolio</a>.</p>
+      <p>Below are some of my recent work.Check out my full <a className='font-bold' href="#" target="_blank">Project Catalog</a>.</p>
       <p>Have a project you would like to discuss?</p>
-      <p><a href="#">Reach out</a> and lets make something great together</p>
+      <p><a href="#" className='font-bold'>Reach out</a> and lets make something great together</p>
       <br/>
       <div className='grid grid-cols-3 m-2 gap-2'>
         <ProjectCard /> 
