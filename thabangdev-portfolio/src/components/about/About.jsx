7f498dd6f07d524526808ecf36ab6725c2715524
thabangDev77 <phoshokothabang20@gmail.com>
2025-12-01 22:59:57 +0200

    chore: improve the layout of the about me section and add an image

--- thabangdev-portfolio/src/components/about/About.jsx
+++ thabangdev-portfolio/src/components/about/About.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
+import aboutImage from '../../assets/images/about.jpg'
 
 function About() {
     return (
         <section>
+            <h2>About Me</h2>
+
             <div>
-                <h2>About Me</h2>
-                <p>Hi, I'm Thabang, a dedicated student with a strong interest in full stack web development. I'm passionate about building complete web applications; from crafting responsive front-end interfaces to designing robust back-end systems. I enjoy the challenge of turning ideas into interactive digital experiences that are both functional and visually appealing.</p>
-                <br/>
-                <p>Currently, I'm focused on expanding my skills in technologies like HTML, CSS, JavaScript, Python, and Node.js, while diving deeper into frameworks like React and Express. I love learning by doing; whether it's working on personal projects, contributing to collaborative teams, or exploring open-source code to understand how real-world systems are built.</p>
-                <br/>
-                <p>As I grow in my journey as a developer, I'm excited to connect with others, learn continuously, and create impactful solutions. Whether it's a sleek portfolio site or a full-featured web app, I believe in writing clean code and building with purpose.</p>  
-                <button>Discover More About Me</button>            
+              <div>
+                  <p>Hi, I'm Thabang, a dedicated student with a strong interest in full stack web development. I'm passionate about building complete web applications; from crafting responsive front-end interfaces to designing robust back-end systems. I enjoy the challenge of turning ideas into interactive digital experiences that are both functional and visually appealing.</p>
+                  <br/>
+                  <p>Currently, I'm focused on expanding my skills in technologies like HTML, CSS, JavaScript, Python, and Node.js, while diving deeper into frameworks like React and Express. I love learning by doing; whether it's working on personal projects, contributing to collaborative teams, or exploring open-source code to understand how real-world systems are built.</p>
+                  <br/>
+                  <p>As I grow in my journey as a developer, I'm excited to connect with others, learn continuously, and create impactful solutions. Whether it's a sleek portfolio site or a full-featured web app, I believe in writing clean code and building with purpose.</p>  
+              </div>
+
+              <div>
+                  <img src={aboutImage} alt="about me image" style={{ width: '400px', height: '300px'}} /> 
+              </div>
             </div>
-            <div>
-                <img src="#" alt="#" width="300" height="300" /> 
-            </div>
+            <button>Discover More About Me</button>            
         </section>
   )
 }
