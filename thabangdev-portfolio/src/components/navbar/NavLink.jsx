875f16cb43af12a0e71b9c7b53b67f6d25aa3909
thabangDev77 <phoshokothabang20@gmail.com>
2025-12-02 20:21:34 +0200

    feat: update navigation links to use react-scroll for smooth scrolling

--- thabangdev-portfolio/src/components/navbar/NavLink.jsx
+++ thabangdev-portfolio/src/components/navbar/NavLink.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-scroll';
 
 function NavLink() {
   return (
     <ul className='flex justify-around p-2 border-2 rounded-xl'>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/exprtise">Expertise</Link></li>
-        <li><Link to="/blog">Blog</Link></li>
+        <li><Link to="home" smooth={true} duration={500} offset={-70}>Home</Link></li>
+        <li><Link to="about-me" smooth={true} duration={500} offset={-70}>About</Link></li>
+        <li><Link to="projects" smooth={true} duration={500} offset={-70}>Projects</Link></li>
+        <li><Link to="expertise" smooth={true} duration={500} offset={-70}>Expertise</Link></li>
+        <li><Link to="blog" smooth={true} duration={500} offset={-70}>Blog</Link></li>
     </ul>
   )
 }
