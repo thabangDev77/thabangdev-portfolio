764dc2d188641ae34f5a1f70d3645deb49b9e37f
thabangDev77 <phoshokothabang20@gmail.com>
2025-12-02 18:18:34 +0200

    chore: restructure the post component to improve styling

--- thabangdev-portfolio/src/components/articles/Post.jsx
+++ thabangdev-portfolio/src/components/articles/Post.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
+import postImage from '../../assets/images/blog.jpg'
 
 function Post() {
   return (
     <div>
+      <img src={postImage} alt='post image' style={{ height: '150px', width: '150px'}} />
       <div>
-          <img src="#" alt="#" height="150" width="150"/>
           <h3>Blog Title</h3>
           <p>Summary/snippet of the blog post</p>
+          <button>Read more</button>
       </div>
-      
-      <button>Read more...</button>
     </div>
   )
 }
