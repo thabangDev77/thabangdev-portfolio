2b741bbe68242c1efb7cf2af63e6d83d1072ac9b
thabangDev77 <phoshokothabang20@gmail.com>
2025-12-02 18:25:23 +0200

    style: display the post card content with grid

--- thabangdev-portfolio/src/components/articles/Post.jsx
+++ thabangdev-portfolio/src/components/articles/Post.jsx
@@ -3,9 +3,9 @@ import postImage from '../../assets/images/blog.jpg'
 
 function Post() {
   return (
-    <div>
-      <img src={postImage} alt='post image' style={{ height: '150px', width: '150px'}} />
-      <div>
+    <div className='grid grid-cols-3 m-4 border-2 rounded-xl'>
+      <img className='col-span-1' src={postImage} alt='post image' style={{ height: '150px', width: '150px'}} />
+      <div className='col-span-2'>
           <h3>Blog Title</h3>
           <p>Summary/snippet of the blog post</p>
           <button>Read more</button>
