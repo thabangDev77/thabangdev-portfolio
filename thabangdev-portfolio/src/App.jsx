d6566e19f35d419ddf116e54a806c14622d32e60
thabangDev77 <phoshokothabang20@gmail.com>
2026-01-31 12:23:45 +0200

    chore: add a light and dark mode button

--- thabangdev-portfolio/src/App.jsx
+++ thabangdev-portfolio/src/App.jsx
@@ -7,20 +7,30 @@ import Expertise from './components/expertise/Expertise';
 import Blog from './components/articles/Blog';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+
 
 function App() {
-  return (
-    <main>
-      <Navbar />
-      <Home />
-      <About />
-      <Project />
-      <Expertise />
-      <Blog />
-      <Contact />
-      <Footer />
-    </main>
-  )
+
+    const darkMode = <FontAwesomeIcon icon={faMoon} size="lg" />
+    const lightMode = <FontAwesomeIcon icon={faSun} size="lg" />   
+    return (
+        <main>
+            <div>
+                {darkMode}
+                {lightMode}
+            </div>
+            <Navbar />
+            <Home />
+            <About />
+            <Project />
+            <Expertise />
+            <Blog />
+            <Contact />
+            <Footer />
+        </main>
+    )
 }
 
 export default App
