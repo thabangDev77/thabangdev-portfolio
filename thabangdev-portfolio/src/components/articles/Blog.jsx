90609c7aa799a88c01a2e8fb690d60fccf7784c1
thabangDev77 <phoshokothabang20@gmail.com>
2026-01-31 17:18:59 +0200

    refactor: map over the incoming data and display the posts

--- thabangdev-portfolio/src/components/articles/Blog.jsx
+++ thabangdev-portfolio/src/components/articles/Blog.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import Post from './Post';
+import blogData from '../../data/blogsData';
 
 function Blog() {
-  return (
-    <section id='blog' className='flex flex-col justify-center items-center my-20'>
-      <h2 className='text-4xl font-bold mb-4'>My Blog</h2>
-      <p>Please check out some of the articles I have published, give me feedback and suggestions</p>
-      <Post />
-      <Post />
-      <Post />
-      <p className='font-bold italic border-x border-b p-2 rounded-2xl'>Other featured blogs</p>
-    </section>
+
+    const dataElements = blogData.map((post) => <Post 
+            key={post.id}
+            img={post.image} 
+            title={post.title}
+            date={post.date} 
+            description={post.description}
+            link={post.link}
+            />
+        )
+
+    return (
+        <section id='blog' className='flex flex-col justify-center items-center my-20'>
+        <h2 className='text-4xl font-bold mb-4'>My Blog</h2>
+        <p>Please check out some of the articles I have published, give me feedback and suggestions</p>
+        
+        <div className='grid grid-cols-3 m-2 gap-2'>
+            {dataElements.slice(0, 3)}
+        </div>
+        
+        <p className='font-bold italic border-x border-b p-2 rounded-2xl'>Other featured blogs</p>
+        </section>
   )
 }
 
