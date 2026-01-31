e81dab2f8af69d3a50b2a4b148adba8b50f60456
thabangDev77 <phoshokothabang20@gmail.com>
2026-01-31 16:43:36 +0200

    feat: map over the incoming data and display the projects

--- thabangdev-portfolio/src/components/projects/Project.jsx
+++ thabangdev-portfolio/src/components/projects/Project.jsx
@@ -1,22 +1,32 @@
 import React from 'react'
 import ProjectCard from './ProjectCard'
 import { Link } from 'react-scroll';
+import projectsData from '../../data/projectsData';
 
 function Project() {
-  return (
-    <section id='projects'  className='flex flex-col justify-center items-center gap-2 my-20 px-4'>
-      <h2 className='text-4xl font-bold'>Recent Projects</h2>
-      <p>Below are some of my recent work.Check out my full <a className='font-bold' href="#" target="_blank">Project Catalog</a>.</p>
-      <p>Have a project you would like to discuss?</p>
-      <p><Link to="contact-me" smooth={true} duration={500} offset={-70} className='font-bold'>Reach out</Link> and lets make something great together</p>
-      <br/>
-      <div className='grid grid-cols-3 m-2 gap-2'>
-        <ProjectCard /> 
-        <ProjectCard /> 
-        <ProjectCard />
-      </div>
-      <button className='font-bold border-2 rounded-3xl p-2'><a href="#">See More Projects</a></button>
-    </section>
+
+    const dataElements = projectsData.map((project) => <ProjectCard 
+        key={project.id}
+        img={project.image} 
+        title={project.title} 
+        description={project.description}
+        technologies={project.technologies}
+        link={project.link}
+        />
+    )
+
+    return (
+        <section id='projects'  className='flex flex-col justify-center items-center gap-2 my-20 px-4'>
+            <h2 className='text-4xl font-bold'>Recent Projects</h2>
+            <p>Below are some of my recent work.Check out my full <a className='font-bold' href="#" target="_blank">Project Catalog</a>.</p>
+            <p>Have a project you would like to discuss?</p>
+            <p><Link to="contact-me" smooth={true} duration={500} offset={-70} className='font-bold'>Reach out</Link> and lets make something great together</p>
+            <br/>
+            <div className='grid grid-cols-3 m-2 gap-2'>
+                {dataElements}
+            </div>
+            <button className='font-bold border-2 rounded-3xl p-2'><a href="#">See More Projects</a></button>
+        </section>
   )
 }
 
