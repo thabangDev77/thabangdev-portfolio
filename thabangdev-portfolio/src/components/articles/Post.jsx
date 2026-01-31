950096ee4dc76b58a19fe9c8ed643d6de82c9a80
thabangDev77 <phoshokothabang20@gmail.com>
2026-01-31 17:18:32 +0200

    refactor: update the post component to accept props

--- thabangdev-portfolio/src/components/articles/Post.jsx
+++ thabangdev-portfolio/src/components/articles/Post.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import postImage from '../../assets/images/blog.jpg'
 
-function Post() {
-  return (
-    <div className='grid grid-cols-4 m-4 border-2 rounded-xl p-2'>
-      <img className='col-span-1' src={postImage} alt='post image' style={{ height: '150px', width: '150px', border: '1px solid black', borderRadius: '10px'}} />
-      <div className='col-span-3'>
-          <h3 className='text-xl font-bold'>Blog Title</h3>
-          <p className='text-sm italic'>Summary/snippet of the blog post</p>
-          <button className='font-bold border-2 mt-15 ml-96 rounded-3xl p-2'>Read more</button>
-      </div>
-    </div>
+
+function Post(props) {
+    return (
+        <div className='grid grid-cols-4 m-4 border-2 rounded-xl p-2'>
+            <img className='col-span-1' src={props.img} alt='post image' style={{ height: '150px', width: '150px', border: '1px solid black', borderRadius: '10px'}} />
+            <div className='col-span-3'>
+                <div className='flex justify-between items-center'>
+                    <h3 className='text-xl font-bold'>{props.title}</h3>
+                    <span>{props.date}</span>
+                </div>
+                <p className='text-sm italic'>{props.description}</p>
+                <button className='font-bold border-2 mt-15 ml-96 rounded-3xl p-2'><a href={props.link}>Read more</a></button>
+            </div>
+        </div>
   )
 }
 
