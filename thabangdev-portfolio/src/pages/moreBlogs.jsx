d427cd354f7953fd39b77fd0bfa96d12e861a0c1
thabangDev77 <phoshokothabang20@gmail.com>
2026-01-31 17:21:38 +0200

    refactor: update the more blogs page to display all of the blogs dynamically

--- thabangdev-portfolio/src/pages/moreBlogs.jsx
+++ thabangdev-portfolio/src/pages/moreBlogs.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 import Navbar from '../components/navbar/Navbar'
 import Post from '../components/blogs/Post'
 import Footer from '../components/footer/Footer'
+import blogData from '../data/blogsData'
 
 function moreBlogs() {
+
+    const dataElements = blogData.map((post) => <Post 
+            key={post.id}
+            img={post.image} 
+            title={post.title}
+            date={post.date} 
+            description={post.description}
+            link={post.link}
+            />
+        )
+
     return (
         <>
             <Navbar />  
@@ -12,7 +24,7 @@ function moreBlogs() {
                     <h2>My Blog</h2>
                     <p>Please check out my write ups, give me feedback and suggestions</p>
                     <div>
-                        <Post />
+                        {dataElements}
                     </div>
 
                 </section>
