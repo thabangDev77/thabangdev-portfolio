5156dadfc390051db0d4d599fe51d49826302962
thabangDev77 <phoshokothabang20@gmail.com>
2026-03-24 21:57:44 +0200

    refactor: improve the layout and apearance of the expertise section

--- thabangdev-portfolio/src/components/expertise/Expertise.jsx
+++ thabangdev-portfolio/src/components/expertise/Expertise.jsx
@@ -7,37 +7,37 @@ function Expertise() {
     <section id="expertise" className='flex flex-col justify-center items-center gap-2 my-20 px-4'>
         <h2 className='expertise-heading'>Expertise</h2>
         <p>
-          With a solid foundation in data structures and algorithms, I approach
-          challenges with a structured mindset. My growing proficiency in
-          JavaScript, HTML, and CSS has enabled me to build dynamic and
-          responsive web applications; and has further led me Learn more about Full-Stack Development.
+            With a solid foundation in data structures and algorithms, I approach
+            challenges with a structured mindset. My growing proficiency in
+            JavaScript, HTML, and CSS has enabled me to build dynamic and
+            responsive web applications; and has further led me Learn more about Full-Stack Development.
         </p>
 
       {/* Expertise Pillars */}
-      <div className='grid grid-cols-3 gap-2 my-4'>
-        <article className='border-2 rounded-2xl p-2'>
-          <h3 className='text-xl font-semibold'>Problem Solving</h3>
-          <p className='mt-2'>
-            I enjoy breaking down complex problems into manageable components
-            and building logical, maintainable solutions.
-          </p>
-        </article>
+      <div className='descriptions grid grid-cols-3 gap-2 my-4'>
+            <article className='border-2 rounded-2xl p-2'>
+                <h3 className='text-xl font-semibold text-orange-600'>Problem Solving</h3>
+                <p className='mt-2'>
+                    I enjoy breaking down complex problems into manageable components
+                    and building logical, maintainable solutions.
+                </p>
+            </article>
 
-        <article className='border-2 rounded-2xl p-2'>
-          <h3 className='text-xl font-semibold'>Frontend Engineering</h3>
-          <p className='mt-2'>
-            I focus on building clean, accessible, and responsive user
-            interfaces with modern frontend technologies.
-          </p>
-        </article>
+            <article className='border-2 rounded-2xl p-2'>
+                <h3 className='text-xl font-semibold text-orange-600'>Frontend Engineering</h3>
+                <p className='mt-2'>
+                    I focus on building clean, accessible, and responsive user
+                    interfaces with modern frontend technologies.
+                </p>
+            </article>
 
-        <article className='border-2 rounded-2xl p-2'>
-          <h3 className='text-xl font-semibold'>Structured Learning</h3>
-          <p className='mt-2'>
-            I continuously improve by building projects, refining fundamentals,
-            and applying best practices in real-world scenarios.
-          </p>
-        </article>
+            <article className='border-2 rounded-2xl p-2'>
+                <h3 className='text-xl font-semibold text-orange-600'>Structured Learning</h3>
+                <p className='mt-2'>
+                    I continuously improve by building projects, refining fundamentals,
+                    and applying best practices in real-world scenarios.
+                </p>
+            </article>
       </div>
 
       {/* Tech Stack */}
