20489e97280688562c36a5d1a4d5bc9eb9c1e183
thabangDev77 <phoshokothabang20@gmail.com>
2026-03-24 21:42:19 +0200

    refactor: improve the layout and appearance of the about me section

--- thabangdev-portfolio/src/components/about/About.jsx
+++ thabangdev-portfolio/src/components/about/About.jsx
@@ -9,11 +9,11 @@ function About() {
 
         <div className='grid grid-cols-5 gap-10 items-center ml-10'>
           <div className='col-span-3'>
-              <p>Hi, I'm Thabang, a dedicated student with a strong interest in full stack web development. I'm passionate about building complete web applications; from crafting responsive front-end interfaces to designing robust back-end systems. I enjoy the challenge of turning ideas into interactive digital experiences that are both functional and visually appealing.</p>
+              <p>Hi, I'm <span>Thabang</span>, a dedicated student with a strong interest in <span>full stack web development</span>. I'm passionate about building complete web applications; from crafting responsive front-end interfaces to designing robust back-end systems. I enjoy the challenge of turning ideas into interactive digital experiences that are both functional and visually appealing.</p>
               <br/>
-              <p>Currently, I'm focused on expanding my skills in technologies like HTML, CSS, JavaScript, Python, and Node.js, while diving deeper into frameworks like React and Express. I love learning by doing; whether it's working on personal projects, contributing to collaborative teams, or exploring open-source code to understand how real-world systems are built.</p>
+              <p>Currently, I'm focused on expanding my skills in technologies like <span>HTML, CSS, JavaScript, Python, and Node.js</span>, while diving deeper into frameworks like <span>React and Express</span>. I love learning by doing; whether it's working on personal projects, contributing to collaborative teams, or exploring open-source code to understand how real-world systems are built.</p>
               <br/>
-              <p>As I grow in my journey as a developer, I'm excited to connect with others, learn continuously, and create impactful solutions. Whether it's a sleek portfolio site or a full-featured web app, I believe in writing clean code and building with purpose.</p>  
+              <p>As I grow in my journey as a developer, I'm excited to connect with others, learn continuously, and create impactful solutions. Whether it's a sleek portfolio site or a full-featured web app, I believe in <span>writing clean code</span> and <span>building with purpose.</span></p>  
           </div>
 
           <div className='col-span-2 flex justify-center mr-20'>
