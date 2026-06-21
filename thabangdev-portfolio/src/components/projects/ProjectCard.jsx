c7705054fa95cdc15558971b79f59ba22aa3546f
thabangDev77 <phoshokothabang20@gmail.com>
2026-06-21 20:51:35 +0200

    style: enhance the desktop view

--- thabangdev-portfolio/src/components/projects/ProjectCard.jsx
+++ thabangdev-portfolio/src/components/projects/ProjectCard.jsx
@@ -5,7 +5,7 @@ function ProjectCard(props) {
     <div className='project-card'>
         {/* add a relevant alt text for each projects image */}
         <img src={props.image} alt='project photo'/>
-        <strong className='text-lg italic'>{props.title}</strong>
+        <strong className='project-title text-lg italic'>{props.title}</strong>
         <p>{props.description}</p>
         <p className='font-bold'>Technologies: {props.technologies.join(', ')}</p>
         <button className='view-project-btn'><a href={props.link} target="_blank" rel="noopener noreferrer">View Project</a></button>
