c9e1568904f5ee2ba9cf7e56f323364a83db87bf
thabangDev77 <phoshokothabang20@gmail.com>
2026-06-24 21:38:10 +0200

    chore: add classes to the post component and remove the styling from the componet itself

--- thabangdev-portfolio/src/components/articles/Post.jsx
+++ thabangdev-portfolio/src/components/articles/Post.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 
 function Post(props) {
     return (
-        <div className='grid grid-cols-4 m-4 border-2 rounded-xl p-2'>
-            <img className='col-span-1' src={props.img} alt='post image' style={{ height: '150px', width: '150px', border: '1px solid black', borderRadius: '10px'}} />
-            <div className='col-span-3'>
-                <div className='flex justify-between items-center'>
+        <div className='post'>
+            <img className='' src={props.img} alt='post image'/>
+            <div className=''>
+                <div className='post-heading-date-container'>
                     <h3 className='text-xl font-bold'>{props.title}</h3>
-                    <span>{props.date}</span>
+                    <span className='date'>{props.date}</span>
                 </div>
                 <p className='text-sm italic'>{props.description}</p>
                 <button className='read-blog'><a href={props.link}>Read more</a></button>
