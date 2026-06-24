1c516c1cb06065c6f002134a1204e1fa061b7d6c
thabangDev77 <phoshokothabang20@gmail.com>
2026-06-24 21:54:11 +0200

    style: enhance the layout for larger screens

--- thabangdev-portfolio/src/components/articles/Post.jsx
+++ thabangdev-portfolio/src/components/articles/Post.jsx
@@ -7,7 +7,7 @@ function Post(props) {
             <img className='' src={props.img} alt='post image'/>
             <div className=''>
                 <div className='post-heading-date-container'>
-                    <h3 className='text-xl font-bold'>{props.title}</h3>
+                    <h3 className='post-heading text-xl font-bold'>{props.title}</h3>
                     <span className='date'>{props.date}</span>
                 </div>
                 <p className='text-sm italic'>{props.description}</p>
